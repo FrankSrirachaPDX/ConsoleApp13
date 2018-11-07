46044771e59f44f119c7308f7c73233d20db06e5
FrankSrirachaPDX <42188580+FrankSrirachaPDX@users.noreply.github.com>
2018-11-07 13:06:31 -0800

    Add files via upload
    Updated and fixed code.
    Put class methods in the class file.

---
+++ ConsoleApp12/ConsoleApp12/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    public class Class1 : Program
+    {
+        public int[] Index = { 2, 4, 6, 8 };
+        public int Num;
+
+        public static Class1 Half(Class1 holder, int i)
+        {
+
+            holder.Num = holder.Index[i] / 2;
+
+
+            return holder;
+        }
+
+        public static Class1 Double(Class1 holder, int i)
+        {
+            holder.Num = holder.Index[i] * 2;
+
+            return holder;
+        }
+
+        public static Class1 Power(Class1 holder, int i)
+        {
+            holder.Num = holder.Index[i] * holder.Index[i];
+
+            return holder;
+        }
+
+    }
+}
---
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 holder = new Class1();
+            string temp;
+            int i;
+
+            Console.WriteLine("Enter a number that corresponds to the Class's array index");
+
+            temp = Console.ReadLine();
+            i = Convert.ToInt16(temp);
+
+            Console.WriteLine(holder.Index[i]);
+
+            
+            holder =  Class1.Half(holder, i);
+            Console.WriteLine("Halved");
+            Console.WriteLine(holder.Num);
+
+            holder = Class1.Double(holder, i);
+            Console.WriteLine("Doubled");
+            Console.WriteLine(holder.Num);
+
+            holder = Class1.Power(holder, i);
+            Console.WriteLine("Squared");
+            Console.WriteLine(holder.Num);
+
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
